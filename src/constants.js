ec95ca2d7f098f4432d4575b7dffe4b417930b15
Sourav-Bhattacharya <sourav132003@gmail.com>
2024-08-28 00:52:35 +0530

    Updates

---
+++ src/constants.js
@@ -0,0 +1,50 @@
+
+export const services = [
+    {
+        title: "Cybersecurity Enthusiast",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Software Engineer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Film Lover",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+];
+
+export const name = 'Sourav';
+
+export const experiences = [
+    {
+        'company': 'Redverse Reality',
+        'role': 'Full Stack Intern',
+        'duration': 'July 2024 - Present',
+        'logo': '',
+        'points': [
+            'Involved in dynamic contributions to web development projects, actively influencing and improving the website.',
+            'Developed multiple shared components to simplify tasks for fellow developers, boosting efficiency and teamwork.',
+            'Enhanced product performance through significant contributions to backend development, optimizing essential components for improved efficiency 📈.',
+            'Implemented data security for the company’s product to provide role-based access control ⚙️.'
+        ],
+        'url': 'https://bhk.ai',
+    },
+    {
+        'company': 'THECYBERHOST',
+        'role': 'Cybersecurity Intern',
+        'duration': 'August 2023 - September 2023',
+        'logo': 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fthe-cyberhost.com%2Fassets%2Fimages%2Fredlight-logo.png&tbnid=lrPCJl4CClUBwM&vet=10CAIQxiAoAGoXChMI4Mvd8caViAMVAAAAAB0AAAAAEAY..i&imgrefurl=https%3A%2F%2Fthe-cyberhost.com%2F&docid=yVE2MeK4HB0AxM&w=1920&h=1080&itg=1&q=cyberhost%20logo&ved=0CAIQxiAoAGoXChMI4Mvd8caViAMVAAAAAB0AAAAAEAY',
+        'points': [
+            'Gained experience with android application penetration testing.',
+            'Worked on testing the application security for various application via offensive testing following white-hat protocols.'
+        ],
+        'url': 'https://www.the-cyberhost.com',
+    },
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = process.env.REACT_APP_EMAIL_JS_SERVICE_ID;
+export const EMAIL_JS_TEMPLATE_ID = process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID;
+export const EMAIL_JS_PUBLIC_KEY = process.env.REACT_APP_EMAIL_JS_PUBLIC_KEY;
